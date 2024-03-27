6aff712b3028bf5bbc7718d46ad660f8cc360b59
Thuhuyen <151049217+ntth0603@users.noreply.github.com>
2024-03-27 22:27:09 +0700

    Add test to Git repo

---
+++ Test Suites/Login_TestSuite.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login_TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>77564013-0882-4d18-ad42-416dc71fb49b</testSuiteGuid>
+   <testCaseLink>
+      <guid>bec6227e-3a70-4bea-a5f4-3193e9add6d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_Test</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>0e3c675b-1224-4add-9c65-d0410c9c3381</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data_Login</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>0e3c675b-1224-4add-9c65-d0410c9c3381</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>user</value>
+         <variableId>bdf90bf4-58a5-471e-8389-31322550daa3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>0e3c675b-1224-4add-9c65-d0410c9c3381</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>pass</value>
+         <variableId>5ec0e21e-4c23-46f5-93db-c303762bee2a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
